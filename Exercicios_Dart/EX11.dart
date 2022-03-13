f2041bc44801db4199d89d2376be74125becef65
Nickolas Daniel dos Santos Bento <nickolas_daniel@hotmail.com>
2022-03-13 19:32:05 -0300

    ex 11 e ex 12

---
+++ Exercicios_Dart/EX11.dart
@@ -0,0 +1,9 @@
+void main() {
+  
+  int num = 2;
+  
+  for (int i = 1; i < 11; i++) {
+    print('a tabuada do número $num é: ${i * num}');
+  }
+  
+}
---
+++ Exercicios_Dart/EX12.dart
@@ -0,0 +1,15 @@
+void main() {
+  
+  int num = 5;
+  int a   = 1;
+  int b   = 8;
+  
+  
+  if (b > a){
+  for (int i = b; i > (a-1); i--) {
+    print('a tabuada do número $num é: ${i*num}');
+  }
+  
+  }
+  
+}
