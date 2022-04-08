a5350277389e59c289b5bffd405c149452502ff6
Nickolas Daniel dos Santos Bento <nickolas_daniel@hotmail.com>
2022-04-08 00:21:08 -0300

    Terminando do exercicio 16

--- Exercicios_Dart/EX16.dart
+++ Exercicios_Dart/EX16.dart
@@ -1,23 +1,27 @@
-/*16. Armazenar dez números na memória do computador. Exibir os valores na ordem inversa à da digitação.*/
-void main() {
-  var Carro c1 = Carro("Fusca");
-  var Carro c2 = Carro("Brasilia");
-  var Carro c3 = Carro("Chevete");
-  
-  final carros = [c1, c2, c3];
-//   carros.add(c1);
-//   carros.add(c2);
-//   carros.add(c3);
-  
-  print("Lista: $carros, length: ${carros.length}");
+/* 16. Armazenar dez números na memória do computador. 
+ * Exibir os valores na ordem inversa à da digitação.*/
 
-  // Looping com For Each
-  for(Carro c in carros) {
-    print(" Carro: ${c.nome}");
-  }
+void main() {
+  Numero n1 = Numero(2);
+  Numero n2 = Numero(2);
+  Numero n3 = Numero(3);
+  Numero n4 = Numero(10);
+  Numero n5 = Numero(5);
+  Numero n6 = Numero(6);
+  Numero n7 = Numero(7);
+  Numero n8 = Numero(8);
+  Numero n9 = Numero(9);
+  Numero n10 = Numero(20);
+
+  final numeros = [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10];
 
   // Looping com For Convencional
-  for(int i=0; i<=2; i++) {
-    print(" Carro: ${carros[i].nome}");
+  for (int i = 9; i >= 0; i--) {
+    print(" Numero: ${numeros[i].nome}");
   }
+}
+
+class Numero {
+  int nome;
+  Numero(this.nome);
 }
